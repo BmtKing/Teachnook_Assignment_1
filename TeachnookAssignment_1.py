d7420188b9fe32d66c43ecb64e9df8b30d317bdd
Mustafa Nasikwala <62584135+BmtKing@users.noreply.github.com>
2022-11-21 18:18:34 +0530

    Update TeachnookAssignment_1.py

--- TeachnookAssignment_1.py
+++ TeachnookAssignment_1.py
@@ -1,33 +1,39 @@
 
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
- 
-choice = input("Enter choice(1/2/3/4): ")
+while True:
+    
+    choice = input("Enter choice(1/2/3/4): ")
 
+  
+    if choice in ('1', '2', '3', '4'):
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
 
-if choice in ('1', '2', '3', '4'):
+        if choice == '1':
+            sum = a+b
+            print(a, "+", b, "=", sum)
 
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+        elif choice == '2':
+            sub=a-b
+            print(a, "-", b, "=", sub)
 
-    if choice == '1':
-        sum = a+b
-        print(a, "+", b, "=", sum)
+        elif choice == '3':
+            product=a*b
+            print(a, "*", b, "=", product)
 
-    elif choice == '2':
-        sub=a-b
-        print(a, "-", b, "=", sub)
-
-    elif choice == '3':
-        product=a*b
-        print(a, "*", b, "=", product)
-
-    elif choice == '4':
-        divide=a/b
-        print(a, "/", b, "=", divide)                   
-else:
-    print("Invalid Input")
+        elif choice == '4':
+            divide=a/b
+            print(a, "/", b, "=", divide)
+        
+        next_calculation = input("Let's do next calculation? (Y/N): ")
+        if next_calculation.upper == "N":
+          break
+    
+    else:
+        print("Invalid Input")
